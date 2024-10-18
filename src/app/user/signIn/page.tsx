de819082fb7617fe5ccb25dc6fe627e8267b926c
Namprik <wareesa1847@gmail.com>
2024-10-18 07:38:57 +0700

    edit object start position

--- src/app/user/signIn/page.tsx
+++ src/app/user/signIn/page.tsx
@@ -27,9 +27,10 @@ export default function SignIn() {
   return (
     <div className="absolute w-screen min-w-fit h-screen min-h-fit bg-blue-50 flex justify-center items-center">
       <div className="absolute w-1/3 h-full top-0 right-0 bg-yellow-200" />
-      <div className="relative bg-white w-1/2 min-w-[800px] h-[550px] grid grid-cols-2 items-center shadow-signIn">
+
+      <div className="relative bg-white w-[77%] min-w-fit h-[56%] min-h-fit grid grid-cols-2 items-center shadow-signIn">
         {/* Form Section */}
-        <div className="w-full flex flex-col p-8 gap-y-10">
+        <div className="w-full flex flex-col px-[7%] py-[11%] gap-[12%]">
           <h1 className="text-3xl font-bold text-[#212B36]">Sign In</h1>
           <form className="flex flex-col gap-5" onSubmit={onSubmit}>
             <div>
@@ -80,7 +81,7 @@ export default function SignIn() {
             alt="Decorative shapes"
             layout="fill"
             objectFit="cover"
-            className="absolute top-0 right-0"
+            className="absolute object-right-top"
           />
         </div>
       </div>
