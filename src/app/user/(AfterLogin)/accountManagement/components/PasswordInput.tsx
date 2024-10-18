ab990cd4d3e5259eb728ffdf82467ab5547fc44f
Namprik <wareesa1847@gmail.com>
2024-10-18 22:24:15 +0700

    add account management page

---
+++ src/app/user/(AfterLogin)/accountManagement/components/PasswordInput.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+
+interface PasswordProps {
+  id: string;
+  label: string;
+  placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void; // Change the type for onChange to accept event
+}
+
+const PasswordInput: React.FC<PasswordProps> = ({
+  id,
+  label,
+  placeholder,
+  value,
+  onChange, // Accept onChange as a prop
+}) => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible((prevState) => !prevState);
+  };
+
+  return (
+    <div className="grid grid-cols-2 gap-8">
+      <label
+        htmlFor={id}
+        className="block font-medium text-[#111928] dark:text-white my-auto"
+      >
+        {label}
+      </label>
+      <div className="relative">
+        <input
+          id={id} // Use the passed id prop here
+          type={isPasswordVisible ? "text" : "password"}
+          className="h-12 w-full py-3 px-4 bg-white border border-[#DFE4EA] text-[#637381] placeholder:text-[#9CA3AF] rounded-md focus:border-[#3758F9] focus:outline-none"
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+        />
+        <button
+          type="button"
+          onClick={togglePasswordVisibility}
+          className="absolute inset-y-0 end-0 flex items-center z-20 px-3 cursor-pointer text-[#6B7280] rounded-e-md focus:outline-none focus:text-[#3758F9] dark:text-neutral-600 dark:focus:text-blue-500"
+        >
+          {isPasswordVisible ? (
+            <svg
+              className="shrink-0 size-3.5"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"></path>
+              <circle cx="12" cy="12" r="3"></circle>
+            </svg>
+          ) : (
+            <svg
+              className="shrink-0 size-3.5"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M9.88 9.88a3 3 0 1 0 4.24 4.24"></path>
+              <path d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68"></path>
+              <path d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61"></path>
+              <line x1="2" x2="22" y1="2" y2="22"></line>
+            </svg>
+          )}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PasswordInput;
---
+++ src/app/user/(AfterLogin)/accountManagement/page.tsx
@@ -0,0 +1,132 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+import shapes from "../../../assets/SignIn/shapes.svg";
+import TextInput from "./components/TextInput";
+import PasswordInput from "./components/PasswordInput";
+import SelectInput from "./components/SelectInput";
+
+export default function accountManagement() {
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [id, setId] = useState<string>("");
+  const [faculty, setFaculty] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [rePassword, setRePassword] = useState<string>("");
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const signUpData = {
+      firstName,
+      lastName,
+      email,
+      id,
+      faculty,
+      password,
+      rePassword,
+      role: "user",
+    };
+
+    try {
+      console.log("Sign Up Data: ", signUpData);
+      window.location.href = "/user/verifyEmail";
+    } catch (error: unknown) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="min-w-fit h-screen min-h-fit bg-blue-50 flex justify-center items-center">
+      <div className="w-[45%] min-w-fit m-auto space-y-[7%]">
+        <h1 className="font-extrabold text-5xl leading-9 mt-[7%]">
+          Account Management
+        </h1>
+
+        {/* Form Section */}
+        <div className="bg-gray-50 border border-gray-300 p-16 min-w-fit min-h-fit ">
+          <form className="space-y-12" onSubmit={onSubmit}>
+            <div className="px-2 pb-8 pt-2 rounded-lg border-b-2 border-gray-300 space-y-8">
+              <div className="grid grid-cols-2 gap-8">
+                <TextInput
+                  id="firstName"
+                  label="Firstname"
+                  type="text"
+                  placeholder={firstName}
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+                <TextInput
+                  id="lastName"
+                  label="Lastname"
+                  type="text"
+                  placeholder={lastName}
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+                <TextInput
+                  id="email"
+                  label="Chula Email"
+                  type="email"
+                  placeholder={email}
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <TextInput
+                  id="id"
+                  label="Chual ID"
+                  type="text"
+                  placeholder={id}
+                  value={id}
+                  onChange={(e) => setId(e.target.value)}
+                />
+                <SelectInput
+                  id="faculty"
+                  label="Faculty"
+                  placeholder={faculty}
+                  value={faculty}
+                  onChange={setFaculty}
+                />
+              </div>
+              <div className="flex flex-row items-center space-x-5">
+                <label
+                  htmlFor={id}
+                  className="font-medium text-[#111928] dark:text-white"
+                >
+                  Password:
+                </label>
+                <button
+                  className=" text-[#111928] bg-white border border-gray-400 hover:text-[#3758F9] hover:border-[#D1D5DB] font-medium rounded-md h-12 py-3 px-7 w-fit"
+                  onClick={() => {
+                    window.location.href = "/user/resetPassword";
+                  }}
+                >
+                  Change Password
+                </button>
+              </div>
+            </div>
+
+            <div className="flex flex-row-reverse gap-x-[4%]">
+              <button
+                type="submit"
+                className=" text-white bg-blue-400 hover:bg-[#60A5FA] font-medium rounded-md h-12 py-3 px-7 w-fit"
+              >
+                Save
+              </button>
+              <button
+                className=" text-gray-400 bg-white border border-gray-400 hover:text-[#3758F9] hover:border-[#D1D5DB] font-medium rounded-md h-12 py-3 px-7 w-44"
+                onClick={() => {
+                  window.location.href = "/user/signIn";
+                }}
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
