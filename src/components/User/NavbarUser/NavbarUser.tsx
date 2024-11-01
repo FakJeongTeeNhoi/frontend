2f9051e64af1cf89f49e87a51af7ba7a8a6ff4ca
Tinnaung <111551053+Tinnaung@users.noreply.github.com>
2024-11-01 14:59:50 +0700

    Add NavbarUser (#4)

--- src/components/User/NavbarUser/NavbarUser.tsx
+++ src/components/User/NavbarUser/NavbarUser.tsx
@@ -1,29 +1,28 @@
 import Image from "next/image";
 import DefaultButton from "../../Common/Buttons/DefaultButton";
-import AccountBox from "../../Common/AccountBox/AccountBox";
 import { signOut } from "next-auth/react";
+import AccountBox from "@/components/Common/AccountBox/AccountBox";
 
-export type AdminPage = "Dashboard" | "Request" | "Space Management";
-export default function NavbarAdmin({
+export type UserPage = "Search" | "My Reservation";
+export default function NavbarUser({
   username,
   role,
   focus,
 }: {
   username: string;
   role: string;
-  focus: AdminPage;
+  focus: UserPage;
 }) {
   const navigationLinks = [
-    { name: "Dashboard", href: "/dashboard" },
-    { name: "Request", href: "/request" },
-    { name: "Space Management", href: "/spaceManagement" },
+    { name: "Search", href: "/search" },
+    { name: "My Reservation", href: "/myReservation" },
   ];
   return (
     <>
       <nav className="flex items-center justify-between flex-wrap bg-yellow-100 p-6">
         <div className="flex items-center flex-shrink-0 text-white mr-6">
           <Image
-            src="/admin_logo.png"
+            src="/user_logo.png"
             alt="fakjeongteenhoi logo"
             width={300}
             height={50}
@@ -34,7 +33,7 @@ export default function NavbarAdmin({
             {navigationLinks.map((link, index) => (
               <a
                 key={index}
-                href={`/staff/${link.href}`}
+                href={`/user/${link.href}`}
                 className={`block mt-4 lg:inline-block lg:mt-0 ${
                   focus == link.name ? "text-gray-800" : "text-gray-400"
                 } text-xl font-semibold hover:text-gray-600 mr-4`}
@@ -45,11 +44,11 @@ export default function NavbarAdmin({
           </div>
 
           <div className="flex flex-row justify-items-center items-center space-x-12">
-            <AccountBox username={username} role={role} pageType={"Staff"}/>
+            <AccountBox username={username} role={role} pageType={"User"}/>
             <DefaultButton
               label="Log out"
               onClick={() => {
-                signOut({ callbackUrl: "/staff/signIn" });
+                signOut({ callbackUrl: "/user/signIn" });
               }}
             />
           </div>
