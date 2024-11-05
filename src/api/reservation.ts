082c6f5afbd0097fd4d6599fee37ba8093b92228
Sirawit Pulketkij <sirawit.pulketkij@gmail.com>
2024-11-05 10:15:03 +0700

    add reservation

---
+++ src/api/reservation.ts
@@ -0,0 +1,2 @@
+import axios from "axios";
+const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL_RESERVATION;
--- src/api/space.ts
+++ src/api/space.ts
@@ -4,30 +4,30 @@ import axios from "axios";
 const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL_SPACE;
 
 export type SearchParams = {
-    name: string | null;
-    faculty: string | null;
-    start_datetime: Date | null;
-    end_datetime: Date | null;
-    capacity: number | null;
-    latitude_range: Pair<number, number> | null;
-    longitude_range: Pair<number, number> | null;
-}
+  name: string | null;
+  faculty: string | null;
+  start_datetime: Date | null;
+  end_datetime: Date | null;
+  capacity: number | null;
+  latitude_range: Pair<number, number> | null;
+  longitude_range: Pair<number, number> | null;
+};
 
 export async function getAllSpace() {
-    try {
-        const response = await axios.get(`${backendUrl}/api/space`);
-        return response.data;
-    } catch (error) {
-        console.error("Get spaces error:", error);
-        throw error;
-    }
+  try {
+    const response = await axios.get(`${backendUrl}/api/space`);
+    return response.data;
+  } catch (error) {
+    console.error("Get spaces error:", error);
+    throw error;
+  }
 }
 export async function getSpace(searchParams: SearchParams) {
   try {
     const response = await axios.get(`${backendUrl}/api/space`, {
-        params: searchParams
-      });
-  
+      params: searchParams,
+    });
+
     return response.data;
   } catch (error) {
     console.error("Get spaces error:", error);
