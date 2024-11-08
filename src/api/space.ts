d8668321725b731b5d8cd75c3062fc1769555c6d
phumipatc <phumipat.c@outlook.com>
2024-11-08 22:05:40 +0700

    feat: functions for creating space and room

--- src/api/space.ts
+++ src/api/space.ts
@@ -48,6 +48,28 @@ export type GetSpaceData = {
   room_list: GetRoomData[];
 }
 
+export type CreateSpaceData = {
+  ID: number;
+  name: string;
+  description: string;
+  faculty: string;
+  floor: number;
+  building: string;
+  latitude: number;
+  longitude: number;
+  head_staff: string;
+  is_available: boolean;
+  CreatedAt: string;
+  UpdatedAt: string;
+  DeletedAt: string | null;
+  faculty_access_list: string[];
+  opening_day: string[];
+  staff_list: number[];
+  type: string;
+  working_hour: string[];
+  room_list: number[];
+}
+
 export async function getAllSpace() {
   try {
     const response = await axios.get(`${backendUrl}/space/space/search`);
@@ -76,6 +98,57 @@ export async function getSpaceById(spaceId: string) {
     return response.data.space;
   } catch (error) {
     console.error("Get space error:", error);
+    throw error;
+  }
+}
+
+export async function createSpace(space: GetSpaceData) {
+  try {
+
+    // convert GetSpaceData to CreateSpaceData
+    const createSpaceData: CreateSpaceData = {
+      ID: space.ID,
+      name: space.name,
+      description: space.description,
+      faculty: space.faculty,
+      floor: space.floor,
+      building: space.building,
+      latitude: space.latitude,
+      longitude: space.longitude,
+      head_staff: space.head_staff,
+      is_available: space.is_available,
+      CreatedAt: space.CreatedAt,
+      UpdatedAt: space.UpdatedAt,
+      DeletedAt: space.DeletedAt,
+      faculty_access_list: space.faculty_access_list,
+      opening_day: space.opening_day,
+      staff_list: space.staff_list,
+      type: space.type,
+      working_hour: space.working_hour,
+      room_list: [],
+    };
+
+    // loop through the room_list and create each room
+    for (let room of space.room_list) {
+      room = await createRoom(room);
+      createSpaceData.room_list.push(room.ID);
+    }
+
+    const response = await axios.post(`${backendUrl}/space/space`, createSpaceData);
+
+    return response.data.space;
+  } catch (error) {
+    console.error("Create space error:", error);
+    throw error;
+  }
+}
+
+export async function createRoom(room: GetRoomData) {
+  try {
+    const response = await axios.post(`${backendUrl}/space/room`, room);
+    return response.data.room;
+  } catch (error) {
+    console.error("Create room error:", error);
     throw error;
   }
 }
