22ca20a4f947733cb9433edf5004f843f0f8dd0b
Namprik <wareesa1847@gmail.com>
2024-11-10 02:20:29 +0700

    add staff/spaceManagement page

--- src/app/staff/spaceManagement/page.tsx
+++ src/app/staff/spaceManagement/page.tsx
@@ -4,6 +4,12 @@ import NavbarAdmin from "@/components/Staff/NavbarAdmin/NavbarAdmin";
 import { CreateNewStaffButton } from "@/components/Staff/SpaceManagement/CreateNewStaffButton/CreateNewStaffButton";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import SpaceCardSpaceManagement from "@/components/Staff/SpaceCard/SpaceCardSpaceManagement";
+import { SpaceData } from "@/app/user/search/page";
+import { useEffect, useState } from "react";
+import { getAllSpace, GetRoomData, GetSpaceData } from "@/api/space";
+import addIcon from "@/assets/Reservation/gg_add.png";
+import Image from "next/image";
 
 export default function SpaceManagement() {
   const { data: session } = useSession();
@@ -16,6 +22,42 @@ export default function SpaceManagement() {
   const breadcrumbItems = [
     { label: "Space Management", href: "/staff/spaceManagement" },
   ];
+  const [spaceList, setSpaceList] = useState<SpaceData[]>();
+  useEffect(() => {
+    const fetchSpace = async () => {
+      try {
+        const data = await getAllSpace();
+
+        const spaces: SpaceData[] = [];
+        data.forEach((space: GetSpaceData) => {
+          spaces.push({
+            spaceId: space.ID.toString(),
+            name: space.name,
+            description: space.description,
+            workingHours: space.working_hour,
+            latitude: space.latitude,
+            longitude: space.longitude,
+            faculty: space.faculty,
+            floor: space.floor,
+            building: space.building,
+            isAvailable: space.is_available,
+            createAt: new Date(space.CreatedAt),
+            createBy: "",
+            updateAt: new Date(space.UpdatedAt),
+            updateBy: "",
+            opening_day: space.opening_day,
+            faculty_access_list: space.faculty_access_list,
+            room_list: space.room_list,
+          });
+        });
+
+        setSpaceList(spaces);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchSpace();
+  }, []);
 
   return (
     <>
@@ -29,12 +71,34 @@ export default function SpaceManagement() {
           <div className="flex flex-col py-16 px-32">
             <div className="flex flex-row justify-between items-center w-full">
               <Breadcrumb items={breadcrumbItems} />
-              <CreateNewStaffButton
-                onClick={() => {
-                  router.push("/staff/signUp");
-                }}
-              />
+              <div className="flex flex-row space-x-4">
+                <button
+                  className="cursor-pointer bg-blue-400 hover:bg-blue-500 text-white inline-flex items-center font-semibold py-2 px-6 gap-2 rounded"
+                  onClick={() =>
+                    (window.location.href = "/staff/createSpaceInfo")
+                  }
+                >
+                  <Image src={addIcon} alt="add" />
+                  Create New Space
+                </button>
+                <CreateNewStaffButton
+                  onClick={() => {
+                    router.push("/staff/signUp");
+                  }}
+                />
+              </div>
             </div>
+            {spaceList ? (
+              <div className="grid grid-cols-2 gap-x-32 gap-y-8 w-full mt-16">
+                {spaceList.map((space, index) => (
+                  <SpaceCardSpaceManagement space={space} key={index} />
+                ))}
+              </div>
+            ) : (
+              <div className="text-center w-full font-bold text-3xl mt-16">
+                Loading ...
+              </div>
+            )}
           </div>
         </>
       ) : (
---
+++ src/components/Staff/SpaceCard/SpaceCardSpaceManagement.tsx
@@ -0,0 +1,115 @@
+import { SpaceData } from "@/app/user/search/page";
+import LocationIcon from "@/components/Common/Icons/LocationIcon";
+import TimeIcon from "@/components/Common/Icons/TimeIcon";
+import Tag from "@/components/Common/Tag/Tag";
+import Map from "@/components/Staff/SpaceCard/Map";
+import { SpaceWorkingHour } from "@/components/Staff/SpaceCard/SpaceCardDashboard";
+import { formatOpeningDay } from "@/utils/FormatOpeningDay";
+import ButtonWithIcon from "@/components/Common/Buttons/ButtonWithIcon";
+import { DeleteOverlay, DeleteOverlayProps } from "../Modal/DeleteSpaceModal";
+import { useState } from "react";
+import { SuccessOverlay, SuccessOverlayProps } from "../Modal/SuccessModal";
+
+// export type SpacePreviewInfo = {
+//   spaceId: string;
+//   name: string;
+//   description: string;
+//   workingHours: SpaceWorkingHour;
+//   latitude: number;
+//   longitude: number;
+//   faculty: string;
+//   floor: number;
+//   building: string;
+//   isAvailable: boolean;
+// };
+export default function SpaceCard({ space }: { space: SpaceData }) {
+  const today = new Date();
+  const dayIndex = today.getDay();
+
+  const [isDeleteVisible, setDeleteVisible] = useState(false);
+  const [isSuccessVisible, setSuccessVisible] = useState(false);
+
+  // delete modal
+  const DeleteProps: DeleteOverlayProps = {
+    id: "delete-space",
+    onClose: () => setDeleteVisible(false),
+    onConfirm: async () => {
+      try {
+        // delete space here
+        console.log(`delete spaceId : ${space?.spaceId}`);
+        setDeleteVisible(false);
+        setSuccessVisible(true);
+      } catch (error) {
+        console.error("Failed to create reservation:", error);
+      }
+    },
+    name: space ? space.name : "",
+  };
+
+  // success modal
+  const SuccessProps: SuccessOverlayProps = {
+    id: "success-reserved",
+    onClose: () => setSuccessVisible(false),
+    header: "Deleted Space Successful",
+    text: `‘${space ? space.name : ""}’ has completely been deleted`,
+  };
+
+  return (
+    <div className="flex flex-row space-x-8 py-8 px-4 bg-gray-50 border-gray-300 border-2 rounded-md text-gray-800 justify-between">
+      <DeleteOverlay isVisible={isDeleteVisible} deleteProps={DeleteProps} />
+      <SuccessOverlay
+        isVisible={isSuccessVisible}
+        successProps={SuccessProps}
+      />
+      <div className="flex flex-col space-y-2">
+        <div>
+          <Map
+            latitude={space.latitude}
+            longitude={space.longitude}
+            width={100}
+            height={200}
+          />
+        </div>
+        <div className="flex flex-row space-x-4 items-center">
+          <div className="text-2xl font-bold">{space.name}</div>
+          {space.isAvailable ? (
+            <Tag label="Open" color="bg-green-400" />
+          ) : (
+            <Tag label="Closed" color="bg-red-400" />
+          )}
+        </div>
+        <div className="flex flex-col space-y-2 w-full">
+          <div className="flex flex-row items-center space-x-2">
+            <LocationIcon width={30} height={30} color="#FDE68A" />
+            <div className="text-lg font-medium">
+              Faculty of {space.faculty} {space.building}, Floor {space.floor}
+            </div>
+          </div>
+          <div className="flex flex-row items-center space-x-2">
+            <TimeIcon width={30} height={30} color="#FDE68A" />
+            <div className="text-lg font-medium">
+              {space.workingHours[dayIndex]},{" "}
+              {formatOpeningDay(space.opening_day)}
+            </div>
+          </div>
+          <div>{space.description}</div>
+        </div>
+      </div>
+      <div className="flex flex-col space-y-2">
+        <ButtonWithIcon
+          label="View"
+          onClick={() =>
+            (window.location.href = `/staff/spaceManagement/${space.spaceId}`)
+          }
+        />
+        <ButtonWithIcon
+          label="Edit"
+          onClick={() =>
+            (window.location.href = `/staff/space/space/${space.spaceId}`)
+          }
+        />
+        <ButtonWithIcon label="Delete" onClick={() => setDeleteVisible(true)} />
+      </div>
+    </div>
+  );
+}
